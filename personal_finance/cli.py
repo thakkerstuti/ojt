c8385e7e27b7248f9ed36ccd7b06fef803da6239
rupneel <rupneel2006@gmail.com>
2025-11-24 14:04:54 +0530

    login2

--- personal_finance/cli.py
+++ personal_finance/cli.py
@@ -3,3 +3,13 @@ import re
 import os
 import hashlib
 from getpass import getpass
+
+def is_valid_name(name):
+    return name.isalpha() and len(name) >= 3
+
+def is_valid_age(age):
+    return age.isdigit() and 18 <= int(age) <= 80
+
+def is_valid_email(email):
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return re.match(pattern, email) is not None
