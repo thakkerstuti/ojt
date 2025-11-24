29532d3113a812a113abab090d1ec59028e3c053
rupneel <rupneel2006@gmail.com>
2025-11-24 14:06:00 +0530

    login3

--- personal_finance/cli.py
+++ personal_finance/cli.py
@@ -13,3 +13,14 @@ def is_valid_age(age):
 def is_valid_email(email):
     pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     return re.match(pattern, email) is not None
+
+def is_valid_password(password):
+    if len(password) < 8:
+        return False
+    if not re.search(r"\d", password):
+        return False
+    if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        return False
+    if not re.search(r"[A-Za-z]", password):
+        return False
+    return True
